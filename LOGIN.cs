f52cc2c8000a72aeed2fa648c7d466c9c57454d1
Suraj Chand <61387508+Suraj-Chand@users.noreply.github.com>
2021-04-30 14:48:41 +0545

    Add project files.

---
+++ LOGIN.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bus_Ticketing_System_1
+{
+    public partial class LOG : Form
+    {
+        public LOG()
+        {
+            InitializeComponent();
+        }
+
+        private void LOG_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginbtn_Click(object sender, EventArgs e)
+        {
+            main ss = new main();
+
+            ss.Show();
+        }
+    }
+}
