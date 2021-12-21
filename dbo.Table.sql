85b66e9bab32c2e80b992ee20dce9b9e34e93bf6
Suraj Chand <61387508+Suraj-Chand@users.noreply.github.com>
2021-12-21 14:29:19 +0545

    Add files via upload

--- dbo.Table.sql
+++ dbo.Table.sql
@@ -1,5 +1,15 @@
-﻿CREATE TABLE [dbo].[StationsTB]
-(
-	[Station Code] NVARCHAR(50) NOT NULL PRIMARY KEY, 
-    [Station Name] NVARCHAR(MAX) NOT NULL
-)
+﻿CREATE TABLE [dbo].[busdetails] (
+    [busno]           NVARCHAR (50) NOT NULL,
+    [busname]         NVARCHAR (50) NOT NULL,
+    [chooseroute]     NVARCHAR (50) NOT NULL,
+    [departuretime] NVARCHAR (50) NOT NULL,
+    [arrivaltime]   NVARCHAR (50) NOT NULL,
+    [bustype]         NVARCHAR (50) NOT NULL,
+    [noofseats]       NVARCHAR (50) NOT NULL,
+    [Model]           NVARCHAR (50) NOT NULL,
+    [Driver]          NVARCHAR (50) NOT NULL,
+    [conductor]       NVARCHAR (50) NOT NULL,
+    [busstatus]       NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_busdetails] PRIMARY KEY CLUSTERED ([busno] ASC)
+);
+
